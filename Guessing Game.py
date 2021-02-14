c6b2030cd1c6703c7a8685c14262205e09ef7616
Apurba Ghosh <new.to.world2003@gmail.com>
2021-02-14 17:32:41 +0000

    Added all the files to commit

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,20 +1,20 @@
-# import random 
+import random 
 
-# guess = random.randint(0,20)
-# user = int(input("Enter your Choice: "))
-# while True:
-#     if user != guess:
-#         if user < guess:
-#             print("Guess Higher!")
-#             user = int(input("Enter: "))
-#             continue
-#         else:
-#             print("Guess Lower!")
-#             user = int(input("Enter: "))
-#             continue
-#     else:
-#         print(f"Congrats! You've guessed the correct number {guess}. ")
-#         break      
+guess = random.randint(0,20)
+user = int(input("Enter your Choice: "))
+while True:
+    if user != guess:
+        if user < guess:
+            print("Guess Higher!")
+            user = int(input("Enter: "))
+            continue
+        else:
+            print("Guess Lower!")
+            user = int(input("Enter: "))
+            continue
+    else:
+        print(f"Congrats! You've guessed the correct number {guess}. ")
+        break      
 
 
 
